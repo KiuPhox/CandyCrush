891a5de0ec3fd53de0e2d3f5b8839e47143c6374
Nguyễn Phan Anh Tuấn <tuan.nguyenphananh@gmail.com>
2023-07-21 11:04:18 +0700

    Update CandyShuffle.ts
    Add more polygons

--- src/managers/candy-grid/CandyShuffle.ts
+++ src/managers/candy-grid/CandyShuffle.ts
@@ -7,51 +7,67 @@ import { Random } from '../../utils/Random'
 import BoardStateMachine from '../BoardStateMachine'
 import CandyGrid from './CandyGrid'
 
-const ROTATE_TWEEN_DUR = 1000
+const ROTATE_TWEEN_DUR = 2000
 const MOVE_TWEEN_DUR = 500
 const MOVE_TWEEN_DELAY = 500
+
+const DEGREE_ANGLE_OFFSETS = [0, 0, 0, 30, 0, -18, 0, 12.85714286, 0, 0, 0, 0]
 
 class CandyShuffle {
     private static scene: GameScene
 
-    private static rectangle: Phaser.Geom.Rectangle
-    private static circle: Phaser.Geom.Circle
-    private static triangle: Phaser.Geom.Triangle
+    private static shapes: (Phaser.Geom.Polygon | Phaser.Geom.Circle)[]
 
     public static init(scene: GameScene) {
         this.scene = scene
+        this.shapes = []
 
         const center = new Phaser.Math.Vector2(scene.scale.width / 2, scene.scale.height / 2)
 
-        const length = Math.min(
-            GAME_CONFIG.gridWidth * GAME_CONFIG.tileWidth,
-            GAME_CONFIG.gridHeight * GAME_CONFIG.tileHeight
-        )
+        const radius =
+            Math.min(
+                GAME_CONFIG.gridWidth * GAME_CONFIG.tileWidth,
+                GAME_CONFIG.gridHeight * GAME_CONFIG.tileHeight
+            ) / 2
 
-        this.rectangle = new Phaser.Geom.Rectangle(
-            center.x - length / 2,
-            center.y - length / 2,
-            length,
-            length
-        )
+        this.shapes.push(new Phaser.Geom.Circle(center.x, center.y, radius))
 
-        this.circle = new Phaser.Geom.Circle(center.x, center.y, length / 2)
-        this.triangle = new Phaser.Geom.Triangle(
-            center.x,
-            center.y - (length * Math.sqrt(3)) / 4,
-            center.x - length / 2,
-            center.y + (length * Math.sqrt(3)) / 4,
-            center.x + length / 2,
-            center.y + (length * Math.sqrt(3)) / 4
-        )
+        for (let i = 3; i <= 8; i++) {
+            this.shapes.push(this.createPolygon(i, center, radius))
+        }
+    }
+
+    private static createPolygon(
+        numVertices: number,
+        circleCenter: Phaser.Math.Vector2,
+        circleRadius: number
+    ): Phaser.Geom.Polygon {
+        if (numVertices < 3) {
+            throw new Error('A polygon must have at least 3 vertices.')
+        }
+
+        const vertices: Phaser.Math.Vector2[] = []
+        const angleIncrement = (2 * Math.PI) / numVertices
+        const angleOffset = Phaser.Math.DegToRad(DEGREE_ANGLE_OFFSETS[numVertices])
+
+        for (let i = 0; i < numVertices; i++) {
+            const x = circleRadius * Math.cos(i * angleIncrement + angleOffset)
+
+            const y = circleRadius * Math.sin(i * angleIncrement + angleOffset)
+
+            vertices.push(new Phaser.Math.Vector2(x + circleCenter.x, y + circleCenter.y))
+        }
+
+        return new Phaser.Geom.Polygon(vertices)
     }
 
     public static shuffle(candies: Candy[], shuffle: boolean): number {
         BoardStateMachine.getInstance().updateState(BoardState.CREATE)
 
         candies = Random.shuffleArray(candies)
-        const shape = [this.rectangle, this.circle, this.triangle][Random.Int(0, 2)]
+        const shape = this.shapes[Random.Int(0, this.shapes.length - 1)]
         const points = shape.getPoints(candies.length)
+        const oneThousandPoints = shape.getPoints(1001)
 
         for (let i = 0; i < candies.length; i++) {
             const temp = i / candies.length
@@ -90,7 +106,7 @@ class CandyShuffle {
                         onUpdate: (tween: Phaser.Tweens.Tween) => {
                             let index = tween.getValue()
                             if (index > 1) index -= 1
-                            const point = shape.getPoint(index)
+                            const point = oneThousandPoints[Math.round(index * 1000)]
                             candy.setPosition(point.x, point.y)
                         },
                     })
